a12c284502a9a0ead814188b01234207a2065bb5
Luoxuliang <962526394@qq.com>
2018-03-12 10:45:41 +0800

    update

--- app/src/main/java/com/forever/blrweather/MainActivity.java
+++ app/src/main/java/com/forever/blrweather/MainActivity.java
@@ -3,6 +3,14 @@ package com.forever.blrweather;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ *  * @copyright: 2017 
+ *  * @priject BlrWeather
+ *  * @author LuoXuLiang
+ *  * @description:  description 
+ *  * @date: 2018/3/12 上午10:44
+ *  
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
