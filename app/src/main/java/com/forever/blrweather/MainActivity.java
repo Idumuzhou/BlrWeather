dbdca39aa56dbe718e67d9aae1038e613481e15c
Dumuzhou <962526394@qq.com>
2018-03-21 22:14:38 +0800

    update

--- app/src/main/java/com/forever/blrweather/MainActivity.java
+++ app/src/main/java/com/forever/blrweather/MainActivity.java
@@ -2,14 +2,22 @@ package com.forever.blrweather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
 
+public class MainActivity extends BaseAcitvity {
+    @BindView(R.id.tv_one_code)
+    TextView tv_one_code;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
+
     }
+
+
+
 }
